ed857ca27762f921eabff12ad2410047b41b09de
Pui Choi <choipui@gmail.com>
2017-10-14 00:24:17 -0500

    add login error handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Grid, Row, Col, FormGroup, ControlLabel, FormControl, Checkbox, Button} from 'react-bootstrap';
+import { Grid, Row, Col, FormGroup, ControlLabel, FormControl, Checkbox, Button, Alert} from 'react-bootstrap';
 
 import { login } from '../utils/auth';
 
@@ -10,7 +10,7 @@ class Login extends Component {
     super();
 
     this.state = {
-      errorMessage: ''
+      error: false
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,7 +20,7 @@ class Login extends Component {
     e.preventDefault();
     login(this.email.value, this.pw.value)
       .catch(error => {
-        this.setState({ errorMessage: 'Login Failed!' });
+        this.setState({ error: true });
         console.log(error);
       }).then(() => {
         this.email.value = '';
@@ -30,36 +30,39 @@ class Login extends Component {
 
   render () {
     return (
-      <Grid className="login">
-        <h1 className="text-center logo logo-lg">
-          <Link to="/">Grace's Recipes</Link>
-        </h1>
-        <h2 className="login-title hdg-1">Login</h2>
-        { this.state.errorMessage && <p>{this.state.errorMessage}</p> }
-        <form id="loginForm" className="login-form" onSubmit={this.handleSubmit}>
-          <FormGroup>
-            <ControlLabel>Email</ControlLabel>
-            <input
-              className="form-control"
-              type="email"
-              placeholder="Email"
-              name="email"
-              ref={(email) => this.email = email} />
-          </FormGroup>
-          <FormGroup>
-            <ControlLabel>Password</ControlLabel>
-            <input
-              className="form-control"
-              type="password"
-              placeholder="Password"
-              name="pw"
-              ref={(pw => this.pw = pw)} />
-          </FormGroup>
-          <FormGroup>
-            <input className="submit-button" type="submit" value="Login" />
-          </FormGroup>
-        </form>
-      </Grid>
+      <div>
+        { this.state.error && <Alert bsStyle="danger" className="text-center">Login Failed!</Alert>}
+        <Grid className="login">
+          <h1 className="text-center logo logo-lg">
+            <Link to="/">Grace's Recipes</Link>
+          </h1>
+          <h2 className="login-title hdg-1">Login</h2>
+          { this.state.errorMessage && <p>{this.state.errorMessage}</p> }
+          <form id="loginForm" className="login-form" onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <ControlLabel>Email</ControlLabel>
+              <input
+                className="form-control"
+                type="email"
+                placeholder="Email"
+                name="email"
+                ref={(email) => this.email = email} />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Password</ControlLabel>
+              <input
+                className="form-control"
+                type="password"
+                placeholder="Password"
+                name="pw"
+                ref={(pw => this.pw = pw)} />
+            </FormGroup>
+            <FormGroup>
+              <input className="submit-button" type="submit" value="Login" />
+            </FormGroup>
+          </form>
+        </Grid>
+      </div>
     );
   }
 }
